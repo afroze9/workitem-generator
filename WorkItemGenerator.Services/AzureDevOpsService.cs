fa08ae6111412b7811db7e3183dc1a89ef216085
Afroze Amjad <afrozeamjad@gmail.com>
2020-06-09 02:39:54 +0500

    Done #2

---
+++ WorkItemGenerator.Services/AzureDevOpsService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.TeamFoundation.WorkItemTracking.WebApi;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
+using Microsoft.VisualStudio.Services.Client;
+using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.WebApi;
+using System;
+
+namespace WorkItemGenerator.Services
+{
+    public static class AzureDevOpsService
+    {
+        public static void Sample()
+        {
+
+        }
+    }
+}
